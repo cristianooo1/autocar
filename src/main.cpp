431f7b083912d6650b718e289403285d22969138
cristianoo <cristi_iorga02@yahoo.com>
2026-02-15 18:43:45 +0100

    Added function to keep the car bounded in the game window.

--- src/main.cpp
+++ src/main.cpp
@@ -1,90 +1,123 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-
 class Car
 {
-    public:
-        Car() 
-        { 
-            shape.setRadius(50.f); 
-            shape.setFillColor(sf::Color::Black); 
-            shape.setPosition(sf::Vector2f(x, y)); 
-        } 
-        
-        void movev(const sf::Vector2f& velocity) 
-        { 
-            shape.move(velocity); 
-        } 
+public:
+    Car()
+    {
+        shape.setSize(sf::Vector2f(carWidth, carHeight));
+        shape.setFillColor(sf::Color::Black);
+        shape.setPosition(sf::Vector2f(0.f, 0.f));
+    }
 
-        void render(sf::RenderWindow& window)
-        {
-            window.draw(shape);
-        }
-        
-    private:
-        sf::CircleShape shape;
-        float x = 50.f; 
-        float y = 50.f;
+    void moveOffset(sf::Vector2f velocity)
+    {
+        shape.move(velocity);
+        this->vel = velocity.x;
+    }
 
+    void moveAbs(float x, float y)
+    {
+        shape.setPosition(sf::Vector2f(x, y));
+    }
+
+    sf::Vector2f getPos() const
+    {
+        return shape.getPosition();
+    }
+
+    void clampBoundaries(int windowWidth, int windowHeight)
+    {
+        // left wall
+        if (getPos().x < 0)
+            moveAbs(0, getPos().y);
+
+        // right wall
+        if (getPos().x + carWidth > windowWidth)
+            moveAbs(windowWidth - carWidth, getPos().y);
+
+        // top wall
+        if (getPos().y < 0)
+            moveAbs(getPos().x, 0);
+
+        // bottom wall
+        if (getPos().y + carHeight > windowHeight)
+            moveAbs(getPos().x, windowHeight - carHeight);
+    }
+
+    void render(sf::RenderWindow &window)
+    {
+        window.draw(shape);
+    }
+
+private:
+    // sf::CircleShape shape;
+    sf::RectangleShape shape;
+    float carWidth = 50.f;
+    float carHeight = 50.f;
+    float vel = 2.f;
 };
-
 
 int main()
 {
     unsigned int cellY = 25;
     unsigned int cellX = 24;
-    
+    unsigned int windowSize = 25 * 24;
 
-    sf::RenderWindow window(sf::VideoMode({cellY*cellX, cellY*cellX}), "autocar", sf::Style::Titlebar);
+    int iteration = 0;
 
+    sf::RenderWindow window(sf::VideoMode({cellY * cellX, cellY * cellX}), "autocar", sf::Style::Titlebar);
 
     Car car;
-    sf::Vector2f velocity(10.f, 10.f);
-
+    sf::Vector2f velocity(1.f, 1.f);
 
     while (window.isOpen())
     {
-        while(const std::optional event = window.pollEvent())
+        while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
-                window.close(); 
-
-
+                window.close();
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
-                {
-                    car.movev(sf::Vector2f(0.f, -velocity.y));
-                    std::cout << "W is pressed" << std::endl;
-                }
+            {
+                car.moveOffset(sf::Vector2f(0.f, -velocity.y));
+                std::cout << "W is pressed" << std::endl;
+            }
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
-                {
-                    car.movev(sf::Vector2f(-velocity.x, 0.f));
-                    std::cout << "A is pressed" << std::endl;
-                }
+            {
+                car.moveOffset(sf::Vector2f(-velocity.x, 0.f));
+                std::cout << "A is pressed" << std::endl;
+            }
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
-                {
-                    car.movev(sf::Vector2f(0.f, velocity.y));
-                    std::cout << "S is pressed" << std::endl;
-                }
+            {
+                car.moveOffset(sf::Vector2f(0.f, velocity.y));
+                std::cout << "S is pressed" << std::endl;
+            }
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
-                {
-                    car.movev(sf::Vector2f(+velocity.x, 0.f));
-                    std::cout << "D is pressed" << std::endl;
-                }
-
-
-
+            {
+                car.moveOffset(sf::Vector2f(+velocity.x, 0.f));
+                std::cout << "D is pressed" << std::endl;
+            }
         }
 
         window.clear(sf::Color::Green);
         car.render(window);
-        window.display();
-        std::cout << "window size x " << window.getSize().x << "and y " << window.getSize().y << std::endl;
+        car.clampBoundaries(windowSize, windowSize);
 
+        iteration++;
+        if (iteration == 500)
+        {
+            float currX = car.getPos().x;
+            float currY = car.getPos().y;
+            std::cout << "curr car pos x=" << currX << " and pos y=" << currY << std::endl;
+            iteration = 0;
+        }
+        window.display();
+        // std::cout << "window size x " << window.getSize().x << "and y " << window.getSize().y << std::endl;
     }
 
     return 0;
